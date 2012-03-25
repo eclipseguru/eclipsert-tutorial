04255879bc44dde72dd2ce7132b93c648904466c
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-03-25 18:47:24 +0200

    update headers and add license file
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- hello.console/src/hello/console/ProcessGreetingsCommand.java
+++ hello.console/src/hello/console/ProcessGreetingsCommand.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2012 AGETO Service GmbH and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/org/documents/edl-v10.html.
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ *******************************************************************************/
+/*******************************************************************************
+ * This is sample code. Use at your own risk. It comes WITHOUT any warranty.
+ * Released to public domain. Please copy & paste as you wish.
+ *
+ * Initial Contribution: Gunnar Wagenknecht
+ *******************************************************************************/
 package hello.console;
 
 import hello.job.HelloJobProviderComponent;
@@ -26,9 +43,6 @@ import org.kohsuke.args4j.Argument;
  * A command for scheduling the processing job
  */
 public class ProcessGreetingsCommand extends Command {
-
-	/** TEST_LOCK */
-	private static final String GLOBAL_LOCK_NAME = "testLock";
 
 	@Argument(index = 0, usage = "The path to the context in which the job should be executed. This is used for multi-tenency purposes. The default is '/' which is suitable for the demo.", required = false, metaVar = "CONTEXT-PATH")
 	protected String contextPath = "/";
--- hello.service/src/hello/service/GreetingService.java
+++ hello.service/src/hello/service/GreetingService.java
@@ -1,14 +1,20 @@
-/**
+/*******************************************************************************
  * Copyright (c) 2012 AGETO Service GmbH and others.
  * All rights reserved.
- *
- * This program and the accompanying materials are made available under the terms of the
- * Eclipse Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Distribution License v1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/org/documents/edl-v10.html.
  *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
- */
+ *******************************************************************************/
+/*******************************************************************************
+ * This is sample code. Use at your own risk. It comes WITHOUT any warranty.
+ * Released to public domain. Please copy & paste as you wish.
+ *
+ * Initial Contribution: Gunnar Wagenknecht
+ *******************************************************************************/
 package hello.service;
 
 import java.util.Collection;
