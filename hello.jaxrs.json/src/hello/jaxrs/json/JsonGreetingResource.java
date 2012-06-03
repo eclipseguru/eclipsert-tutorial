6246fa56efeffcf33610e9fffcc27d3eeeab60ad
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-06-03 22:21:39 +0200

    update copyright header
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- hello.jaxrs.json/src/hello/jaxrs/json/JsonGreetingResource.java
+++ hello.jaxrs.json/src/hello/jaxrs/json/JsonGreetingResource.java
@@ -1,19 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2012 AGETO Service GmbH and others.
  * All rights reserved.
- *  
- * This program and the accompanying materials are made available under the 
+ *
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v1.0 which accompanies this distribution,
  * and is available at https://www.eclipse.org/org/documents/edl-v10.html.
  *
  * Contributors:
  *     Gunnar Wagenknecht - initial API and implementation
- *******************************************************************************/
-/*******************************************************************************
- * This is sample code. Use at your own risk. It comes WITHOUT any warranty.
- * Released to public domain. Please copy & paste as you wish.
- *
- * Initial Contribution: Gunnar Wagenknecht
  *******************************************************************************/
 package hello.jaxrs.json;
 
